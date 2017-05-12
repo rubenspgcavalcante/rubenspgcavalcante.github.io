77724aaf2ff028c8acd86dc24221464b2ea222b4
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2017-05-12 17:01:23 +0200

    new build

--- app/commons/modules/core/index.js
+++ app/commons/modules/core/index.js
@@ -1,5 +1,4 @@
 import store from "./store/store";
 import AppRouter from "./components/App.jsx";
-import ActionFactory from "./factories/actions.factory";
 
-module.exports = {store, AppRouter, ActionFactory};
+module.exports = { store, AppRouter };
